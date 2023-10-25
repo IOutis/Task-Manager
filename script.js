bb3ba514c5ee5bbf4fe0aa3690253b8af631d295
Mohd Mushtaq <126546049+IOutis@users.noreply.github.com>
2023-10-25 18:29:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,26 @@ function addTask() {
             <span>${taskText} (Due: ${dueDate.toLocaleString()})</span>
             <button onclick="markAsCompleted(this)">Complete</button>
         `;
-        taskList.appendChild(taskItem);
+
+        const taskList = document.getElementById('taskList');
+        const taskItems = taskList.children;
+        let insertIndex = taskItems.length;
+
+        // Find the correct position to insert the new task
+        for (let i = 0; i < taskItems.length; i++) {
+            const itemDueDate = new Date(taskItems[i].querySelector('span').textContent.match(/\(([^)]+)\)/)[1]);
+            if (dueDate < itemDueDate) {
+                insertIndex = i;
+                break;
+            }
+        }
+
+        // Insert the new task at the correct position
+        if (insertIndex === 0) {
+            taskList.prepend(taskItem);
+        } else {
+            taskItems[insertIndex - 1].after(taskItem);
+        }
 
         // Schedule a notification for the task's due date
         scheduleNotification(taskText, dueDate);
@@ -21,6 +40,7 @@ function addTask() {
         dateTimePicker.value = '';
     }
 }
+
 
 function markAsCompleted(button) {
     const taskItem = button.parentElement;
